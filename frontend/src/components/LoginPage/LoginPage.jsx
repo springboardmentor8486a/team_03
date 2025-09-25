05383ee771bb1aa35708ad1a189fa1d9d322c669
Sandeep Pal <sandeeppal6926@gmail.com>
2025-09-25 22:17:30 +0530

    Update LoginPage.jsx

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
    
     setLoading(true);
     try {
-      const response = await fetch("https://localhost:5000/api/users/login", {
+      const response = await fetch("http://localhost:5000/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
