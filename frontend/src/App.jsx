cdf254e9cc98d38156940ff599d44f7b9050dff8
Ashok Gupta <maturiashokgupta@gmail.com>
2025-10-22 11:41:03 +0530

    Updated map functionality with new improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import AdminDashboard from "./pages/AdminDashboard";
 import ViewDetails from "./components/Dashboard/ViewDetails";
 import WorkProgressDashboard from "./pages/WorkProgressDashboard";
 import MyReports from "./components/Dashboard/MyReports";
+import AreaMapPage from "./pages/AreaMapPage";
+import AdminMapPage from "./pages/AdminMapPage";
 
 function App() {
   return (
@@ -16,17 +18,17 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login/*" element={<LoginForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
+        <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
         <Route path="/profile" element={<Profile />} /> {/* ✅ add route */}
         <Route path="/view-details" element={<ViewDetails />} /> {/* ✅ add view details route */}
         <Route path="/my-reports" element={<MyReports />} /> {/* ✅ add reports route */}
         <Route path="/reportissue" element={<ReportIssue />} /> {/* ✅ add report issue route */}
         <Route path="/work-progress" element={<WorkProgressDashboard />} />
-        
+        <Route path="/map" element={<AreaMapPage />} />
+        <Route path="/admin/map" element={<AdminMapPage />} />
       </Routes>
     </BrowserRouter>
   );
-  
 }
 
 export default App;
